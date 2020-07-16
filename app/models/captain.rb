90dc4504a6c2e64b0dc04cd12c23141676a82036
matthewlazewski <matthew.lazewski@gmail.com>
2020-07-16 10:49:19 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,24 +1,25 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats 
 
   def self.catamaran_operators
-    # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
-    # includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end
 
   def self.motorboat_operators
-    # includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
   end
 
   def self.talented_seafarers
-    # where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+    where("id IN (?)", sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
   def self.non_sailors
-    # where.not("id IN (?)", self.sailors.pluck(:id))
+    where.not("id IN (?)", sailors.pluck(:id))
   end
 
 end
